c2e242e117c2676183b0ca8c0774ba93ef65f68c
DANIIL MIKHEEV <125924070+m3tlo@users.noreply.github.com>
2023-10-02 15:56:12 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,83 +1,35 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-// import firebase from "firebase/app";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import auth from './auth'
+import info from './info'
+import category from './category'
+import record from './record'
 
-Vue.use(VueRouter);
+Vue.use(Vuex)
 
-
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    
-    {
-      path: '/login',
-      name: 'login',
-      meta: {layout: 'empty'},
-      component: () => import("@/views/UserLogin.vue")
+export default new Vuex.Store({
+  state: {
+    error: null
+  },
+  mutations: {
+    setError(state, error) {
+      state.error = error
     },
-    {
-      path: '/register',
-      name: 'register',
-      meta: {layout: 'empty' , auth: true},
-      component: () => import("@/views/UserRegister.vue")
-    },
-    {
-      path: "/",
-      name: "home",
-      meta: {layout: 'main', auth: true},
-      component: () => import('@/views/HomePage.vue'),
-    },
-    
-    {
-      path: '/categories',
-      name: 'categories',
-      meta: {layout: 'main', auth: true},
-      component: () => import("@/views/MyCategories.vue")
-    },
-    {
-      path: '/record',
-      name: 'record',
-      meta: {layout: 'main', auth: true},
-      component: () => import("@/views/MyRecord.vue")
-    },
-    {
-      path: '/detail',
-      name: 'detail',
-      meta: {layout: 'main', auth: true},
-      component: () => import("@/views/DetailRecord.vue")
-    },
-    {
-      path: '/history',
-      name: 'history',
-      meta: {layout: 'main', auth: true},
-      component: () => import("@/views/MyHistory.vue")
-    },
-    {
-      path: '/planning',
-      name: 'planning',
-      meta: {layout: 'main', auth: true},
-      component: () => import("@/views/MyPlanning.vue")
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      meta: {layout: 'main', auth: true},
-      component: () => import("@/views/UserProfile.vue")
-    },
-  ]
-});
-// router.beforeEach((to, from, next) => {
-//   const currentUser = firebase.auth().currentUser
-//   const requireAuth = to.matched.some(record => record.meta.auth)
-  
-//   if(requireAuth && !currentUser) {
-//     next('/login?message=login')
-//   } else {
-//     next()
-//   }
-// })
-
-export default router
-
-
+    clearError(state) {
+      state.error = null
+    }
+  },
+  actions: {
+    async fetchCurrency() {
+      const key = process.env.VUE_APP_FIXER
+      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`)
+      return await res.json()
+    }
+  },
+  getters: {
+    error: s => s.error
+  },
+  modules: {
+    auth, info, category, record
+  }
+})
